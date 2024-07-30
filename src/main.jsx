74bf54a42b058e927ba647831ac9a5e69c40cc43
rf-gul <rf-gul@pop-os.localdomain>
2024-07-30 16:11:29 +0500

    added vercel analytics and speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // import {AppProvider} from "./context/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain="dev-rfgul.us.auth0.com"
-
     // client id for bitxcoin.vercel.app
     // clientId="RrDDoX3no1VO4eXru9kSqGcG9ellsclR"
 
-
     // client id for localhost
-    // clientId="q3z8KOkgkQ6H7hj3iKgSUvUerqYROhRj"
-
-
+    clientId="q3z8KOkgkQ6H7hj3iKgSUvUerqYROhRj"
     // client id for production
-    clientId="Zd6eDVg5AEqNcgdTNhJ9lfU4oGGe4FlM"
+    // clientId="Zd6eDVg5AEqNcgdTNhJ9lfU4oGGe4FlM"
 
     authorizationParams={{
       redirect_uri: window.location.origin,
@@ -27,6 +25,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   >
     <div className="px-6 md:px-12 lg:px-20 xl:px-32 ">
       <App />
+      <Analytics/>
+      <SpeedInsights/>
     </div>
   </Auth0Provider>
 );
