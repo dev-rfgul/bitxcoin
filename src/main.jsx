7f9496a83ebb947ae19c19846f611711c162ced2
rf-gul <rf-gul@pop-os.localdomain>
2024-07-31 20:54:13 +0500

    added ads.txt file

--- src/main.jsx
+++ src/main.jsx
@@ -19,9 +19,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     // clientId="RrDDoX3no1VO4eXru9kSqGcG9ellsclR"
 
     // client id for localhost
-    clientId="q3z8KOkgkQ6H7hj3iKgSUvUerqYROhRj"
+   // clientId="q3z8KOkgkQ6H7hj3iKgSUvUerqYROhRj"
+
+
     // client id for production
-    // clientId="Zd6eDVg5AEqNcgdTNhJ9lfU4oGGe4FlM"
+    clientId="Zd6eDVg5AEqNcgdTNhJ9lfU4oGGe4FlM"
 
     authorizationParams={{
       redirect_uri: window.location.origin,
