485f854bdfe7d9eb395842f0161a869f6ffbe508
rf-gul <rf-gul@pop-os.localdomain>
2024-08-01 12:51:05 +0500

    env file setted up

--- src/main.jsx
+++ src/main.jsx
@@ -3,36 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "react-redux";
-// import store from "./app/store"
+import { store } from "./app/store";
 
-import {store} from "./app/store"
-// import {AppProvider} from "./context/AppContext";
+// Access the environment variable
+const auth0ClientId = import.meta.env.VITE_clientId;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
   <Provider store={store}>
-  <Auth0Provider
-    domain="dev-rfgul.us.auth0.com"
-    // client id for bitxcoin.vercel.app
-    // clientId="RrDDoX3no1VO4eXru9kSqGcG9ellsclR"
-
-    // client id for localhost
-     clientId="q3z8KOkgkQ6H7hj3iKgSUvUerqYROhRj"
-
-
-    // client id for production
-    //clientId="Zd6eDVg5AEqNcgdTNhJ9lfU4oGGe4FlM"
-
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
-    <div className="px-6 md:px-12 lg:px-20 xl:px-32 ">
-      <App />
-      <Analytics/>
-    </div>
-  </Auth0Provider>
+    <Auth0Provider
+      domain="dev-rfgul.us.auth0.com"
+      clientId={auth0ClientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <div className="px-6 md:px-12 lg:px-20 xl:px-32 ">
+        <App />
+        <Analytics />
+      </div>
+    </Auth0Provider>
   </Provider>
 );
